2b08dc9602ea2ef1e67780f3c1f9e1f2df009628
y1 <39407459+plsy1@users.noreply.github.com>
2023-10-26 22:06:28 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -7,19 +7,24 @@ def pause_and_resume_unregistered_torrents(qb_client):
     for torrent in torrents:
         for tracker in torrent.trackers:
             if tracker.msg == "unregistered torrent":
-                print(f"Pausing torrent '{torrent.name}'")
-                torrent.pause()
-                print(f"Resuming torrent '{torrent.name}'")
-                torrent.resume()
-                break;
+                size_gb = torrent.size / (1024**3)
+                if 14 < size_gb < 50:
+                    print(f"Pausing torrent '{torrent.name}'")
+                    torrent.pause()
+                    print(f"Resuming torrent '{torrent.name}'")
+                    torrent.resume()
+                    break;
+                else:
+                    print(f"Deleting torrent '{torrent.name}' (Size: {size_gb:.2f} GB)")
+                    torrent.pause()
+                    torrent.delete(delete_files=True)
+                    break;
 
-
-
-qb = Client(host='', port=8080, username='', password='')
+qb = Client(host='127.0.0.1:8080', port=8080, username='', password='')
 qb.auth_log_in()
 
 
 while True:
     print("----------Start----------")
     pause_and_resume_unregistered_torrents(qb)
-    time.sleep(10)
+    time.sleep(5)
